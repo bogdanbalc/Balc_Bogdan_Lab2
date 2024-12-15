95fe3263629695649a5aa538d53e03252a592170
Bogdan Balc <bogdan.balc@outlook.com>
2024-12-15 23:39:50 +0200

    Laborator6 changes

--- Models/Book.cs
+++ Models/Book.cs
@@ -8,10 +8,13 @@ namespace Balc_Bogdan_Lab2.Models
     {
         public int ID { get; set; }
         [Display(Name = "Book Title")]
+        [Required(ErrorMessage = "The title is required.")]
+        [StringLength(150, MinimumLength = 3, ErrorMessage = "The title must be between 3 and 150 characters long.")]
         public string Title { get; set; }
         public int? AuthorID { get; set; }
         public Author? Author { get; set; }
         [Column(TypeName = "decimal(6, 2)")]
+        [Range(0.01, 500)]
         public decimal Price { get; set; }
         [DataType(DataType.Date)]
         public DateTime PublishingDate { get; set; }
